71cbdc6e327bbfe83a115d682d66d77bd753bfb3
Parinita Das <parinitad008@gmail.com>
2025-03-01 21:47:15 +0530

    router

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,17 @@
+'use client'
+import { useEffect } from "react";
 import Layout from "./Components/Layout";
+import { routes } from "./utils/routes";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push(routes.CATEGORY)
+  }, [])
+
   return (
     <>
     <Layout></Layout>
